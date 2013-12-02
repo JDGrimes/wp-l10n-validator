6aad27d7d704d27422c4add7e8fe3370375cd87c
JDGrimes <jdg@codesymphony.co>
2013-12-02 14:48:38 -0500

    WordPress ignore rules: update to Class::__construct instead of the old 'new Class' convention
    This was overlooked when I decided to ignore class constructor
    arguments via ‘Class::__construct’ instead of ‘new Class’.

--- config/wordpress/ignores.php
+++ config/wordpress/ignores.php
@@ -77,10 +77,10 @@ $parser->add_ignored_functions(
 		'$wpdb->get_results' => true,
 		'$wpdb->get_var'     => true,
 		'$wpdb->get_col'     => true,
-		// New instance calls.
-		'new File_Upload_Upgrader' => true,
-		'new WP_Date_Query'        => true,
-		'new WP_Error'             => array( 1 ),
+		// Static/parent method calls.
+		'File_Upload_Upgrader::__construct' => true,
+		'WP_Date_Query::__construct'        => true,
+		'WP_Error::__construct'             => array( 1 ),
 	)
 );
 
